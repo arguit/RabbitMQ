dd2ebb3bcafef45d4a93df11682fda49ad77582d
arguit <arguit@gmail.com>
2020-10-10 09:32:19 +0200

    Add project files.

---
+++ Receive/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Receive
+{
+    class Program
+    {
+        private const string HOST_NAME = "localhost";
+        private const string QUEUE_NAME = "Test";
+
+        static void Main(string[] args)
+        {
+            // create connection factory with default guest/guest credentials
+            // these credntials are limited to localhost connections
+            var factory = new ConnectionFactory()
+            {
+                HostName = HOST_NAME
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // we must declare queue before we can use it
+                // if not existing then the queue will be created
+                channel.QueueDeclare(
+                    queue: QUEUE_NAME,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                // create consumer and hook-up the message received event                
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (sender, e) =>
+                {
+                    // display body and CorrelationID the from received message
+                    var body = e.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    var basicProperties = e.BasicProperties;
+                    
+                    Console.WriteLine($"Message received [CorrelationId:{ basicProperties.CorrelationId }]: { message }");
+                };
+
+                // register the created consumer
+                channel.BasicConsume(
+                    queue: QUEUE_NAME,
+                    autoAck: true,
+                    consumer: consumer);
+
+                Console.WriteLine("Press any key to exit");
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ Send/Program.cs
@@ -0,0 +1,57 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace Send
+{
+    class Program
+    {
+        private const string HOST_NAME = "localhost";
+        private const string QUEUE_NAME = "Test";
+
+        static void Main(string[] args)
+        {
+            // create connection factory with default guest/guest credentials
+            // these credntials are limited to localhost connections
+            var factory = new ConnectionFactory()
+            {
+                HostName = HOST_NAME
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                // we must declare queue before we can use it
+                // if not existing then the queue will be created
+                channel.QueueDeclare(
+                    queue: QUEUE_NAME,
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                // create a message in a plain text and encoded
+                // version that will be sent to message queue
+                var message = "Hi there RabbitMQ #^_^)/";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                // create basic message properties
+                // can be used for example for CorrelationID propagation
+                var basicProperties = channel.CreateBasicProperties();
+                basicProperties.CorrelationId = Guid.NewGuid().ToString();
+
+                // publish the message to a desired message queue
+                channel.BasicPublish(
+                    exchange: string.Empty,
+                    routingKey: QUEUE_NAME,
+                    basicProperties: basicProperties,
+                    body: body);
+
+                Console.WriteLine($"Message sent [CorrelationID:{ basicProperties.CorrelationId }]: { message }");
+            }
+
+            Console.WriteLine("Pressa any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
